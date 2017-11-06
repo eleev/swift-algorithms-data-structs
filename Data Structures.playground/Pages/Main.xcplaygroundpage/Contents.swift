62984c6342862c93467f1ad2d03b819b270b9064
Astemir Eleev <astemireleev@gmail.com>
2017-11-06 15:44:10 +0300

    Added links to playground pages

--- Data Structures.playground/Pages/Main.xcplaygroundpage/Contents.swift
+++ Data Structures.playground/Pages/Main.xcplaygroundpage/Contents.swift
@@ -2,9 +2,9 @@
 This is a Playground Book that contains examples of common Algorithms and Data Structures implemented using the latest Swift programming language version. The development attempts to fully utilize standard library and protocol-oriented paradigm. The book provides visualizations using SpriteKit and SceneKit frameworks. 📖
  
  Here you can find the following list of data structures:
- - Stack
+ - [Stack](Stack)
+ - [Circular Buffer](CircularBuffer)
  - Queue (in development)
- - Circular Buffer (in development)
  - Linked List (in development)
  */
 
