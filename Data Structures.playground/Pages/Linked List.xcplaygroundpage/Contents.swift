59f9debbb624d3e975f597d3da5d2a81ff4af792
Astemir Eleev <astemireleev@gmail.com>
2017-11-15 19:17:31 +0300

    Added conformance to the following protocols: Sequence, ExpressibleByArrayLiteral, CustomStringConvertable and CustomDebugStringConvertable

--- Data Structures.playground/Pages/Linked List.xcplaygroundpage/Contents.swift
+++ Data Structures.playground/Pages/Linked List.xcplaygroundpage/Contents.swift
@@ -5,7 +5,7 @@ public struct LinkedList<T> {
     // MARK: - Private properties
     
     fileprivate var head: Node<T>?
-    fileprivate var _count: Int
+    fileprivate var _count: Int = 0
     
     // MARK: - Public properties
     
@@ -17,7 +17,12 @@ public struct LinkedList<T> {
     
     public init() {
         head = nil
-        _count = 0
+    }
+    
+    public init<S: Sequence>(sequence: S) where S.Iterator.Element == T {
+        for element in sequence {
+            push(element: element)
+        }
     }
     
     // MARK: - API Methods
@@ -59,6 +64,98 @@ private class Node<T> {
         self.data = data
     }
 }
+
+// MARK: - Conformance to CusomStringConvertable and CustomDebugStringConvertable protocols
+extension LinkedList: CustomStringConvertible, CustomDebugStringConvertible {
+    
+    // MARK: - Conformance to the protocols
+    
+    public var description: String {
+        return composeDescription()
+    }
+    
+    public var debugDescription: String {
+        return composeDescription()
+    }
+    
+    // MARK: - Utliity mehtods
+    
+    private func composeDescription() -> String {
+        var description = "["
+        var lastNode = head
+        
+        while lastNode != nil {
+            description += "\(String(describing: lastNode?.data))"
+            lastNode = lastNode?.next
+            
+            if lastNode != nil {
+                description += ","
+            }
+        }
+        description += "]"
+
+        return description
+    }
+    
+}
+
+
+// MARK: - Conformance to ExpressibleByArrayLiteral protocol that allows to treat the LinkedList as an array when initializing it
+extension LinkedList: ExpressibleByArrayLiteral {
+    
+    public init(arrayLiteral elements: T...) {
+        for element in elements {
+            push(element: element)
+        }
+    }
+}
+
+/// Custom generic struct that conforms to IteratorProtocol. The structure receives an instnace of head during initialization pohase that allows us to iterate through the elements in the linked ilst.
+public struct LinkedListIterator<T>: IteratorProtocol {
+    
+    // MARK: - Properties
+    
+    public typealias Element = T
+    private var head: Node<Element>?
+    
+    // MARK: - Initializers
+    
+    fileprivate init(head: Node<T>?) {
+        self.head = head
+    }
+    
+    // MARK: - Conformacne to the protocol
+    
+    public mutating func next() -> T? {
+        guard let uHead = head else { return nil }
+        let item = uHead.data
+        head = uHead.next
+        return item
+    }
+}
+
+// MARK: - Conformance to
+extension LinkedList: Sequence {
+    public typealias Iterator = LinkedListIterator<T>
+    
+    public func makeIterator() -> LinkedListIterator<T> {
+        return LinkedListIterator<T>(head: head)
+    }
+}
+
+
+// MARK: - Usage
+
+var list: LinkedList = [1,2,4,5]
+debugPrint(list)
+
+list.pop()
+debugPrint(list)
+
+var newList = LinkedList<Int>(sequence: list)
+debugPrint(newList)
+newList.pop()
+debugPrint(newList)
 
 
 //: [Next](@next)
