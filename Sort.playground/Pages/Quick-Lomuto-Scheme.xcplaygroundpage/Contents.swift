915102db7938a9f4a5bc57703d1f21e43cdcaaef
Astemir Eleev <astemireleev@gmail.com>
2018-03-11 15:41:51 +0300

    Implemented Quick Sort (Lomuto Scheme)

---
+++ Sort.playground/Pages/Quick-Lomuto-Scheme.xcplaygroundpage/Contents.swift
@@ -0,0 +1,44 @@
+//: [Previous](@previous)
+
+import Foundation
+
+extension Array where Element: Comparable {
+    
+    
+    static func quickSort(array: inout [Element], lowest: Int, highest: Int) {
+        if lowest < highest {
+            let pivot = Array.partition(array: &array, lowest: lowest, highest: highest)
+            
+            Array.quickSort(array: &array, lowest: lowest, highest: pivot - 1)
+            Array.quickSort(array: &array, lowest: pivot + 1, highest: highest)
+        }
+    }
+    
+
+    private static func partition(array: inout [Element], lowest: Int, highest: Int) -> Int {
+        let pivot = array[highest]
+        var i = lowest
+        
+        for j in lowest..<highest {
+            if array[j] <= pivot {
+                array.swapAt(i, j)
+                i += j
+            }
+        }
+        
+        array.swapAt(i, highest)
+        return i
+    }
+    
+}
+
+
+// Usage:
+
+var nums = [1, 5, 6, 3, 2, 7, 8, 5, 8, 4, 2, 9, 0]
+Array.quickSort(array: &nums, lowest: 0, highest: nums.count - 1)
+
+print(#function + " sorted array : " , nums)
+
+
+//: [Next](@next)
