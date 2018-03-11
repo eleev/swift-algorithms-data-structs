9bd036f2284e91ae25baa8ffdc28a8db2fe4fb1c
Astemir Eleev <astemireleev@gmail.com>
2018-03-11 15:48:06 +0300

    Refactored partitioning method name

--- Sort.playground/Pages/Quick-Lomuto-Scheme.xcplaygroundpage/Contents.swift
+++ Sort.playground/Pages/Quick-Lomuto-Scheme.xcplaygroundpage/Contents.swift
@@ -4,18 +4,16 @@ import Foundation
 
 extension Array where Element: Comparable {
     
-    
     static func quickSort(array: inout [Element], lowest: Int, highest: Int) {
         if lowest < highest {
-            let pivot = Array.partition(array: &array, lowest: lowest, highest: highest)
+            let pivot = Array.partitionLomuto(array: &array, lowest: lowest, highest: highest)
             
             Array.quickSort(array: &array, lowest: lowest, highest: pivot - 1)
             Array.quickSort(array: &array, lowest: pivot + 1, highest: highest)
         }
     }
-    
 
-    private static func partition(array: inout [Element], lowest: Int, highest: Int) -> Int {
+    private static func partitionLomuto(array: inout [Element], lowest: Int, highest: Int) -> Int {
         let pivot = array[highest]
         var i = lowest
         
@@ -36,7 +34,7 @@ extension Array where Element: Comparable {
 // Usage:
 
 var nums = [1, 5, 6, 3, 2, 7, 8, 5, 8, 4, 2, 9, 0]
-Array.quickSort(array: &nums, lowest: 0, highest: nums.count - 1)
+Array.quickSort(array: &nums, lowest: 1, highest: nums.count - 1)
 
 print(#function + " sorted array : " , nums)
 
