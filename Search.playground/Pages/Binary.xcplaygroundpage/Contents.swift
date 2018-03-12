022a554ac2f7b7478423ce019d3c9afce86c6067
Astemir Eleev <astemireleev@gmail.com>
2018-03-12 15:58:32 +0300

    Typo fixes

--- Search.playground/Pages/Binary.xcplaygroundpage/Contents.swift
+++ Search.playground/Pages/Binary.xcplaygroundpage/Contents.swift
@@ -27,15 +27,17 @@ extension Array where Element: Comparable {
 
 //: Usage
 
-// NOTES: 
+// NOTES:
 // The following comparison will not work because two Strings cannot be compared with > or < operators - in order to determine lexical order they need to be lexically comapred. You can use the following construction to achieve such result:
 // strings.first?.compare(strings.last) == ComparisonResult.orderedAscending
 
 // Another solution is to implement custom coparison closure and inject it to the binarySearch function (this is TODO thing)
 // And the last approach is to override exisiting comparison operator for String type so two strings will be lexically compared with each other
 
-let strings = ["steve", "Apple", "bar", "foo", "juce", "iPad", "macOS", "book", "universal"]
+let strings = ["Steve", "Apple", "bar", "foo", "juce", "iPad", "macOS", "book", "universal"]
 var index = strings.binarySearch(key: "universal")
 
 let numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67]
 index = numbers.binarySearch(key: 67)
+
+//: [Next](@next)
