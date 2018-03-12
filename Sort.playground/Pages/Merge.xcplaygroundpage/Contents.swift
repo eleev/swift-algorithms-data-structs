f418c8284d32913926b51976801c940c27cd454a
Astemir Eleev <astemireleev@gmail.com>
2018-03-12 14:57:32 +0300

    Added ComparisonClosure parameter and refactored Merge Sort functions

--- Sort.playground/Pages/Merge.xcplaygroundpage/Contents.swift
+++ Sort.playground/Pages/Merge.xcplaygroundpage/Contents.swift
@@ -19,19 +19,25 @@ The final phase is Conbine - it merges the sorted sublists into a sorted sequenc
 // MARK: - Array extension that adds support for Merge sort algorithm
 extension Array where Element: Comparable {
 
+    // MARK: - Typealiases
+    
+    public typealias ComparisonClosure = (Element, Element) -> Bool
+    
+    // MARK: - Methods
+    
     /// Sorts an array of Comparable elements using Merge sort algorithm
     ///
     /// - Parameter list: is an Array of Comparable elements
     /// - Returns: the same sorted Array
-    public func mergeSort(_ list: [Element]) -> [Element] {
+    public static func mergeSort(_ list: [Element], order sign: ComparisonClosure) -> [Element] {
 
         if list.count < 2 { return list }
         let center = (list.count) / 2
         
-        let leftMergeSort = mergeSort([Element](list[0..<center]))
-        let rightMergeSort = mergeSort([Element](list[center..<list.count]))
+        let leftMergeSort = mergeSort([Element](list[0..<center]), order: sign)
+        let rightMergeSort = mergeSort([Element](list[center..<list.count]), order: sign)
         
-        return merge(left: leftMergeSort, right: rightMergeSort)
+        return merge(left: leftMergeSort, right: rightMergeSort, order: sign)
     }
 
     /// Helper method that performs Conquer and Combine stages for the Merge sort algorithm
@@ -40,7 +46,7 @@ extension Array where Element: Comparable {
     ///   - lhalf: is an Array containing left half of the target array that needs to be sorted
     ///   - rhalf: is an Array containing right hald of the target array that needs to be sorted
     /// - Returns: a Combined array of Comparable elements
-    private func merge(left lhalf: [Element], right rhalf: [Element]) -> [Element] {
+    private static func merge(left lhalf: [Element], right rhalf: [Element], order sign: ComparisonClosure) -> [Element] {
         
         var leftIndex = 0
         var rightIndex = 0
@@ -53,10 +59,13 @@ extension Array where Element: Comparable {
             let leftElement = lhalf[leftIndex]
             let rightElement = rhalf[rightIndex]
             
-            if leftElement < rightElement {
+            let leftGreatherThanRight = sign(leftElement, rightElement)
+            let leftSmallerThanRight = !leftGreatherThanRight
+            
+            if leftGreatherThanRight {
                 temp.append(leftElement)
                 leftIndex += 1
-            } else if leftElement > rightElement {
+            } else if leftSmallerThanRight {
                 temp.append(rightElement)
                 rightIndex += 1
             } else {
@@ -74,6 +83,11 @@ extension Array where Element: Comparable {
     }
     
 }
+
+//: Usage
+
+var nums = [1, 5, 6, 3, 2, 7, 8, 5, 8, 4, 2, 9, 0]
+nums = Array.mergeSort(nums, order: <)
 
 
 //: [Next](@next)
