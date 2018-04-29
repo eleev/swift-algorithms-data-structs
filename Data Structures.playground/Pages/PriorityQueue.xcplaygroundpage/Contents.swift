64c940440f64a11f23e67b6580861b08b30574f6
Astemir Eleev <astemireleev@gmail.com>
2018-04-29 14:48:18 +0300

    Added usage sample for PriorityQueue

---
+++ Data Structures.playground/Pages/PriorityQueue.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//: Usage: 
+
+var queue = PriorityQueue<Int>(elements: [2, 1, 4, 3, 5], order: >)
+queue.enqueue(9)
+
+print(#function + " queue: " , queue)
+
+//: [Next](@next)
