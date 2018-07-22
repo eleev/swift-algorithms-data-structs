8faac2ad6cf334abac27b7f88a4cd4ed7a96499c
Astemir Eleev <astemireleev@gmail.com>
2018-07-22 19:35:41 +0300

    Refactored Stack playground file

--- Data Structures.playground/Pages/Stack.xcplaygroundpage/Contents.swift
+++ Data Structures.playground/Pages/Stack.xcplaygroundpage/Contents.swift
@@ -134,27 +134,6 @@ debugPrint("pushed 10: ", stack)
 import SpriteKit
 import PlaygroundSupport
 
-class StackElementNode: SKSpriteNode {
-    
-    init() {
-        let size = CGSize(width: 50, height: 150)
-        
-        let randomHue = CGFloat(arc4random_uniform(255))
-        let randomSaturation = CGFloat(arc4random_uniform(255))
-        let randomBrightness = CGFloat(arc4random_uniform(255))
-        let color = SKColor(hue: randomHue, saturation: randomSaturation, brightness: randomBrightness, alpha: 1.0)
-            
-        super.init(texture: SKTexture(imageNamed: "book spine"), color: color, size: size)
-        zRotation = 90 * CGFloat.pi / 180.0
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError(#function + " required initizlier for NSCode has not been implemented")
-    }
-}
-
-
-
 class StackScene: SKScene {
     
     var stackElements: Stack<StackElementNode>! {
